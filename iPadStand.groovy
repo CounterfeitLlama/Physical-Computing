2b576d465ecd7f55292a24187dbdd74cd8638152
Andrew Turley <aturley123@gmail.com>
2016-09-29 10:09:32 -0400

    Added the parameters

--- iPadStand.groovy
+++ iPadStand.groovy
@@ -1,7 +1,7 @@
 import eu.mihosoft.vrl.v3d.parametrics.*;
 
 ArrayList<CSG> makeStand() {
-	LengthParameter thickness = new LengthParameter("Material Thickness", 2, [1, 10]);
+	LengthParameter thickness = new LengthParameter("Material Thickness", 4, [10, 1]);
 
 	CSG bottom = new Cube(150, 50, thickness.getMM()).toCSG()
 	CSG frontMiddle = new Cube(10, 50, thickness.getMM()).toCSG().rotx(60).movez(20).movey(-25)
@@ -11,7 +11,18 @@ ArrayList<CSG> makeStand() {
 
 	bottom = bottom.difference(frontMiddle).difference(frontLeft).difference(frontRight).difference(front)
 
-	return [bottom, frontMiddle, frontLeft, frontRight, front]
+	def listOfParts = [bottom, frontMiddle, frontLeft, frontRight, front]
+	for (int i = 0; i < listOfParts.size(); i++) {
+		int local = i;
+		listOfParts.get(local)
+					.setParameter(thickness)
+					.setRegenerate({
+						return makeStand().get(local)
+					})
+	}
+	
+	return listOfParts
 }
 
+CSGDatabase.clear()
 return makeStand()
